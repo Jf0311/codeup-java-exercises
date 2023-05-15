00912f55041ec56aa22b25ae7600b75e4768c344
Joseph Fuentes <josephkfuentes@gmail.com>
2023-05-15 17:40:27 -0400

    Finished exercise

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -3,38 +3,46 @@ import java.util.Scanner;
 public class ConsoleExercises {
     public static void main(String[] args){
         double pi = 3.14159;
-//        System.out.println(pi);
-//        System.out.printf("The value of pi is approximately, %f", pi);
+        System.out.println(pi);
+        System.out.printf("The value of pi is approximately, %.2f. %n", pi);
+
+        Scanner sc = new Scanner(System.in);
+        int userInput ;
+
+        System.out.println(" Enter Number:  ");
+        userInput = Integer.parseInt(sc.nextLine());
+
+        System.out.println("You entered: " + userInput);
 //
 //        Scanner sc = new Scanner(System.in);
-////        int userInput ;
-////
-////        System.out.println(" Enter Number:  ");
-////        userInput = Integer.parseInt(sc.nextLine());
-////
-////        System.out.println("You entered: " + userInput);
-////
-////        Scanner sc = new Scanner(System.in);
-//
-////        Scanner myObj = new Scanner(System.in);
-//
-//
-//
-//        System.out.println("Enter three different words: ");
-//
-//        // String input
-////        String wordOne = sc.nextLine();   This you can fill up the lines as much as we want
-////        String wordTwo = sc.nextLine();
-////        String wordThree = sc.nextLine();
-//
-//        String wordOne = sc.next();  // You can type all in one line
-//        String wordTwo = sc.next();
-//        String wordThree = sc.next();
-//
-//
-//
-//
-//
+
+//        Scanner myObj = new Scanner(System.in);
+
+
+
+        System.out.println("Enter three different words: ");
+
+        // String input
+//        String wordOne = sc.nextLine();   This you can fill up the lines as much as we want
+//        String wordTwo = sc.nextLine();
+//        String wordThree = sc.nextLine();
+
+        String wordOne = sc.next();  // You can type all in one line
+        String wordTwo = sc.next();
+        String wordThree = sc.next();
+
+
+        Scanner scThree = new Scanner(System.in);
+        System.out.println("Enter a Sentence: ");
+        String ourSentence = scThree.nextLine();
+        System.out.printf("This is a sentence : %s %n ", ourSentence);
+
+
+
+
+
+
+
 //
 //        // Output input by user
 //        System.out.printf("Words are  : %s %n %s %n %s %n", wordOne,wordTwo,wordThree );
@@ -46,15 +54,15 @@ public class ConsoleExercises {
          int length = Integer.parseInt(sctTwo.nextLine());
          int width = Integer.parseInt(sctTwo.nextLine());
 
-        System.out.printf("length is %d, %n , and width is %d,  "  , length, width);
+        System.out.printf("length is %d, %n  and width is %d %n   "  , length, width);
         /* Display the area and perimeter of that classroom.
 The area of a rectangle is equal to the length times the width, and the perimeter of a rectangle is equal
  to 2 times the length plus 2 times the width.*/
 
         int areaOfRectangle = length * width;
         int perimeterOfRectangle = (length * 2) + (width * 2);
-        System.out.println(areaOfRectangle);
-        System.out.println(perimeterOfRectangle);
+        System.out.println("The area of the rectangle is " + areaOfRectangle);
+        System.out.println("The area of the perimeter  is " + perimeterOfRectangle);
 
 
     }
