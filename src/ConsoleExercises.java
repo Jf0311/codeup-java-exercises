0329e8530b545fdbd8675c391a1ade160b6d2bff
Joseph Fuentes <josephkfuentes@gmail.com>
2023-05-15 13:29:35 -0400

    Console Exercise 1s

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -1,6 +1,8 @@
 public class ConsoleExercises {
     public static void main(String[] args){
         double pi = 3.14159;
+        System.out.println(pi);
+        System.out.printf("The value of pi is approximately, %f", pi);
 
 
     }
