3b24d4604ba576460c86f73bd0bb8e40bc6d1d45
Joseph Fuentes <josephkfuentes@gmail.com>
2023-05-17 15:38:34 -0400

    Finished exercise

--- src/Bob.java
+++ src/Bob.java
@@ -1,9 +1,38 @@
-//public class Bob {
-//    public static void main (String[] args) {
-//
-//        String bobResponse =
-//
-//
-//       }
-//    }
+import java.util.Scanner;
+
+public class Bob {
+    public static void main (String[] args) {
+
+//        String bobResponse ;
+//        Scanner input = new Scanner(System.in);
+
+//        System.out.println("Hello , my name is Bob  ");
+//        bobResponse = input.next();
+
+
+        Scanner sc = new Scanner(System.in);
+        String myResponse;
+        Boolean confirm = true;
+
+        do {
+            System.out.println("Hi , I am Bob ");
+            myResponse = sc.nextLine();
+
+            if (myResponse.isEmpty()) {
+                System.out.println("Bob: Fine. Be that way!");
+            } else if (myResponse.endsWith("?")) {
+                System.out.println("Bob: Sure.");
+            } else if (myResponse.endsWith("!")){
+                System.out.println("Bob: Whoa, chill out!");
+            } else {
+                System.out.println("Bob: Whatever.");
+            }
+        } while (!myResponse.equals("bye"));
+
+        System.out.println("Bob: Bye!");
+    }
+}
+
+
+
 
