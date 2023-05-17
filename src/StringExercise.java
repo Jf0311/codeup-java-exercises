a6cda6ad1afd410dc4f20057f60a408e1a0bb5fa
Joseph Fuentes <josephkfuentes@gmail.com>
2023-05-17 13:26:23 -0400

    completed part 1

---
+++ src/StringExercise.java
@@ -0,0 +1,16 @@
+public class StringExercise {
+    public static void main (String[] args){
+
+        String string = "We don't need no education";
+         string += " We don't need no thought control";
+        System.out.println(string);
+        string = "Check \"this\" out!, \"s inside of \"s!\n";
+        System.out.println(string);
+        string = "In windows, the main drive is usually C:\\\n";
+        System.out.println(string);
+        string = "I can do backslashes \\, double backslashes \\\\,\n" +
+                "and the amazing triple backslash \\\\\\!\n ";
+        System.out.println(string);
+
+    }
+}
