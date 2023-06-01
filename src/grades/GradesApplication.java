90ce2e75ada9a650862f44dccd10f806c5555f95
Joseph Fuentes <josephkfuentes@gmail.com>
2023-06-01 12:53:23 -0400

    exercise completed

--- src/grades/GradesApplication.java
+++ src/grades/GradesApplication.java
@@ -1,7 +1,7 @@
 package grades;
-
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 
 public class GradesApplication {
     public static void main(String[] args) {
@@ -9,31 +9,103 @@ public class GradesApplication {
         HashMap<String, Student> students = new HashMap<>();
 
         // Add students with GitHub usernames directly to the map
-        students.put("john123", createStudent("John", 11, 22,33));
-        students.put("alice456", createStudent("Alice", 95, 85, 92));
-        students.put("bob789", createStudent("Bob", 70, 80, 78));
-        students.put("emma321", createStudent("Emma", 88, 92, 90));
+        students.put("Zach_github1", createStudent("Zach", 85, 90, 92));
+        students.put("Ryan_github2", createStudent("Ryan", 78, 82, 80));
+        students.put("Jessica_github3", createStudent("Jessica", 92, 88, 90));
+        students.put("Fernado_github4", createStudent("Fernando", 79, 85, 92));
+        students.put("Luis_github5", createStudent("Luis", 95, 92, 97));
 
-        // Access and print student information from the map
-        for (Map.Entry<String, Student> entry : students.entrySet()) {
-            String username = entry.getKey();
-            Student student = entry.getValue();
-            System.out.println("GitHub Username: " + username);
-            System.out.println("Student Name: " + student.getName());
-            System.out.println("Average Grade: " + student.getGradeAverage());
+        // Create a scanner to read user input from the console
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Welcome!");
+        System.out.println();
+        System.out.println("Here are the GitHub usernames of our students:");
+        printGitHubUsernames(students);
+
+        String userInput;
+        boolean continueLoop = true;
+
+        do {
             System.out.println();
-        }
+            System.out.println("What student would you like to see more information on?");
+            userInput = scanner.nextLine();
+
+            Student student = students.get(userInput);
+            if (student == null) {
+                System.out.println();
+                System.out.println("Sorry, no student found with the GitHub username of \"" + userInput + "\".");
+            } else {
+                System.out.println();
+                System.out.println("Name: " + student.getName() + " - GitHub Username: " + userInput);
+                System.out.println("Current Average: " + student.getGradeAverage());
+            }
+
+            System.out.println();
+            System.out.println("Would you like to see another student? (yes/no)");
+            userInput = scanner.nextLine();
+
+            if (!userInput.equalsIgnoreCase("yes")) {
+                continueLoop = false;
+            }
+        } while (continueLoop);
+
+        System.out.println();
+        System.out.println("Goodbye, and have a wonderful day!");
+
+        // Close the scanner
+        scanner.close();
     }
 
     private static Student createStudent(String name, int... grades) {
         Student student = new Student(name);
-        for (int grade : grades) {                    //For each loop
+        for (int grade : grades) {
             student.addGrade(grade);
         }
         return student;
     }
-}
 
+    private static void printGitHubUsernames(Map<String, Student> students) {
+        for (String username : students.keySet()) {
+            System.out.print("|" + username + "| ");
+        }
+        System.out.println();
+    }
+}
+//import java.util.HashMap;
+//import java.util.Map;
+//
+//public class GradesApplication {
+//    public static void main(String[] args) {
+//        // Create a HashMap to store students and their GitHub usernames
+//        HashMap<String, Student> students = new HashMap<>();
+//
+//        // Add students with GitHub usernames directly to the map
+//        students.put("john123", createStudent("John", 11, 22,33));
+//        students.put("alice456", createStudent("Alice", 95, 85, 92));
+//        students.put("bob789", createStudent("Bob", 70, 80, 78));
+//        students.put("emma321", createStudent("Emma", 88, 92, 90));
+//
+//        // Access and print student information from the map
+//        for (Map.Entry<String, Student> entry : students.entrySet()) {
+//            String username = entry.getKey();
+//            Student student = entry.getValue();
+//            System.out.println("GitHub Username: " + username);
+//            System.out.println("Student Name: " + student.getName());
+//            System.out.println("Average Grade: " + student.getGradeAverage());
+//            System.out.println();
+//        }
+//    }
+//
+//    private static Student createStudent(String name, int... grades) {
+//        Student student = new Student(name);
+//        for (int grade : grades) {                    //For each loop
+//            student.addGrade(grade);
+//        }
+//        return student;
+//    }
+//}
+//
 
 
 
--- src/util/Input.java
+++ src/util/Input.java
@@ -1,9 +1,11 @@
+
 package util;
 
 import java.util.Scanner;
 
 public class Input {
     private Scanner scanner;
+
     public Input() {
         this.scanner = new Scanner(System.in);
     }
@@ -21,50 +23,48 @@ public class Input {
         int num;
         do {
             System.out.printf("Please enter an integer between %d and %d: ", min, max);
-            while (!scanner.hasNextInt()) {
+            try {
+                num = Integer.valueOf(getString());
+            } catch (NumberFormatException e) {
                 System.out.println("Invalid input. Please enter an integer.");
-                scanner.next();
+                num = min - 1; // Set num to an invalid value to continue the loop
             }
-            num = scanner.nextInt();
-            scanner.nextLine();
         } while (num < min || num > max);
         return num;
     }
 
     public int getInt() {
         System.out.print("Please enter an integer: ");
-        while (!scanner.hasNextInt()) {
+        try {
+            return Integer.valueOf(getString());
+        } catch (NumberFormatException e) {
             System.out.println("Invalid input. Please enter an integer.");
-            scanner.next();
+            return getInt(); // Recursive call until a valid integer is entered
         }
-        int num = scanner.nextInt();
-        scanner.nextLine();
-        return num;
     }
 
     public double getDouble(double min, double max) {
         double num;
         do {
             System.out.printf("Please enter a decimal number between %.2f and %.2f: ", min, max);
-            while (!scanner.hasNextDouble()) {
+            try {
+                num = Double.valueOf(getString());
+            } catch (NumberFormatException e) {
                 System.out.println("Invalid input. Please enter a decimal number.");
-                scanner.next();
+                num = min - 1; // Set num to an invalid value to continue the loop
             }
-            num = scanner.nextDouble();
-            scanner.nextLine();
         } while (num < min || num > max);
         return num;
     }
 
     public double getDouble() {
         System.out.print("Please enter a decimal number: ");
-        while (!scanner.hasNextDouble()) {
+        try {
+            return Double.valueOf(getString());
+        } catch (NumberFormatException e) {
             System.out.println("Invalid input. Please enter a decimal number.");
-            scanner.next();
+            return getDouble(); // Recursive call until a valid double is entered
         }
-        double num = scanner.nextDouble();
-        scanner.nextLine();
-        return num;
     }
 
     public static void main(String[] args) {
